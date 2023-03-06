4a42e6bb25e5ec31911a1517c8bae100284da26c
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-03-06 11:34:46 +0300

    6 attempt

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
